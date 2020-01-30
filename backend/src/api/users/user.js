b76e7f94d1be41d87c1eab1e2ce4b41c3e259781
Thiago Desktop <thiagorbernardo@hotmail.com>
2020-01-30 18:37:13 -0300

    OK

--- backend/src/api/users/user.js
+++ backend/src/api/users/user.js
@@ -1,5 +1,5 @@
 const restful = require('node-restful')
-const bcrypt = require('bcrypt')
+//const bcrypt = require('bcrypt')
 const saltRounds = 10;
 const mongoose = restful.mongoose
 class activities {
@@ -9,7 +9,7 @@ class activities {
         this.correr = correr;
     }
 }
-
+//new Date().toISOString().split('T')[0]
 const userSchema = new mongoose.Schema({
     createdAt: { type: Date, default: Date.now },
     username: { type: String, required: true, minlength: 4, maxlength: 16 },
@@ -19,44 +19,56 @@ const userSchema = new mongoose.Schema({
         activities: [{ name: { type: String }, duration: { type: Number } }],
     }],
 })
-
+/*
 userSchema.pre('save', function (next) {
     this.password = bcrypt.hashSync(this.password, saltRounds);
     next();
 }, function (err) {
     next(err);
 });
-
+*/
 
 module.exports = restful.model('User', userSchema)
-//module.exports = restful.model('Act', actSchema)
-/*const actSchema = new mongoose.Schema({
-    //username: { type: String, required: true },
-    createdAt: { type: Date, required: true, default: Date.now },
-    correr: {type: Number},
-    dormir: {type: Number},
-    andar: {type: Number},
-})*/
 
 
-// POST BODY
+/*
+POST BODY
 
-// {
-// 	"username":"kjasdfha",
-// 	"password": "jhasdfkjhasdf",
-// 	"activities":[{
-// 					"date": "2020-01-30T18:42:46.609Z",
-// 					"activities": [
-// 							{ "name": "Almoçar", "duration": 30 },
-// 							{ "name": "Dormir", "duration": 120 }
-// 					]
-// 			}, 
-// 	{
-// 					"date": "2020-01-29T18:42:46.609Z",
-// 					"activities": [
-// 							{ "name": "Almoçar", "duration": 30 },
-// 							{ "name": "Cagar", "duration": 45 },
-// 							{ "name": "Dormir", "duration": 120 }
-// 					]
-// 	}]
-// }
+{
+	"username":"kjasdfha",
+	"password": "jhasdfkjhasdf",
+	"activities":[{
+					"date": "2020-01-30T18:42:46.609Z",
+					"activities": [
+							{ "name": "Almoçar", "duration": 30 },
+							{ "name": "Dormir", "duration": 120 }
+					]
+			},
+	{
+					"date": "2020-01-29T18:42:46.609Z",
+					"activities": [
+							{ "name": "Almoçar", "duration": 30 },
+							{ "name": "Cagar", "duration": 45 },
+							{ "name": "Dormir", "duration": 120 }
+					]
+	}]
+}*/
+
+/*
+{
+	"username":"username",
+	"password": "senha123",
+	"activities":[{
+					"activities": [
+							{ "name": "Almoçar", "duration": 30 },
+							{ "name": "Dormir", "duration": 120 }
+					]
+			},
+	{
+					"activities": [
+							{ "name": "Almoçar", "duration": 30 },
+							{ "name": "Dormir", "duration": 120 }
+					]
+	}]
+}
+*/
