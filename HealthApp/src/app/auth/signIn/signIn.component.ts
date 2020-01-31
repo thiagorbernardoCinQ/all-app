2668d89fba89f2a4d61c86908a1568cd34e0f5cc
t.bernardo <t.bernardo@cinq.com.br>
2020-01-31 14:26:47 -0300

    starting front

---
+++ HealthApp/src/app/auth/signIn/signIn.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-signIn',
+  templateUrl: './signIn.component.html',
+  styleUrls: ['./signIn.component.css']
+})
+export class SignInComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ HealthApp/src/app/auth/signUp/signUp.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-signUp',
+  templateUrl: './signUp.component.html',
+  styleUrls: ['./signUp.component.css']
+})
+export class SignUpComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
